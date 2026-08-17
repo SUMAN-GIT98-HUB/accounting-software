e4a91e15556027bc60f2abb1983b6d1a7dd007d0
SUMAN-GIT98-HUB <shrestha.sum98@gmail.com>
2026-08-17 17:54:19 +0545

    Move Supabase connection to supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,9 @@
+// Supabase connection
 
+const SUPABASE_URL = 'https://tmdqjsbpgpvpwqryckcb.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_eqidn56P_QNeWZ2ivnicKw_l7SHccTf';
+
+const sb = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
