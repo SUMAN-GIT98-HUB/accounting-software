5d80ea746e87100244051089e02b479447764e4f
SUMAN-GIT98-HUB <shrestha.sum98@gmail.com>
2026-08-21 15:21:01 +0545

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,44 @@
+/* ============================= UTILITIES ============================= */
 
+function toast(msg){
+  const t = document.getElementById('toast');
+  t.textContent = msg;
+  t.classList.add('show');
+
+  clearTimeout(window.__toastTimer);
+
+  window.__toastTimer = setTimeout(
+    ()=>t.classList.remove('show'),
+    2200
+  );
+}
+
+
+function esc(s){
+  return String(s==null?'':s).replace(
+    /[&<>"']/g,
+    c=>({
+      '&':'&amp;',
+      '<':'&lt;',
+      '>':'&gt;',
+      '"':'&quot;',
+      "'":'&#39;'
+    }[c])
+  );
+}
+
+
+function baseCurrency(){
+  return state.meta.currency || 'NPR';
+}
+
+
+function isForeignLedger(ledgerId){
+  const l = ledgerById(ledgerId);
+
+  return !!(
+    l &&
+    l.currency &&
+    l.currency !== baseCurrency()
+  );
+}
